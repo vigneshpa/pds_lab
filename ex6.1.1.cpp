df2d140519446ba90252ba06b735ebe61177816b
vignesh <vignesh-p@outlook.com>
2021-10-28 17:10:32 +0530

    create display

---
+++ ex6.1.1.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+	int data;
+	Node *next;
+};
+
+class List
+{
+public:
+	Node *head;
+	List()
+	{
+		head = NULL;
+	}
+	void create(int arr[], int n)
+	{
+		Node *prev = NULL;
+		for (int i = 0; i < n; i++)
+		{
+			Node *a = new Node();
+			a->data = arr[i];
+			if (!prev)
+			{
+				head = prev = a;
+			}
+			else
+			{
+				prev->next = a;
+				prev = a;
+			}
+		}
+	}
+	void display()
+	{
+		Node *temp = head;
+		while (temp != NULL)
+		{
+			cout << temp->data << ' ';
+			temp = temp->next;
+		}
+	}
+};
+int main()
+{
+	int arr[] = {1, 2, 3, 4, 5};
+	int n = 5;
+	List a;
+	a.create(arr, n);
+	a.display();
+	cout << endl;
+}
