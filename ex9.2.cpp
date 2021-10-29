9f5d5bdb9a781ea5883eee164ce47401d9ad6a46
vignesh <vignesh-p@outlook.com>
2021-10-29 09:29:24 +0530

    improving linear search

--- ex9.2.cpp
+++ ex9.2.cpp
@@ -2,34 +2,27 @@
 using namespace std;
 class linear
 {
-	int arr[10], item;
+	int arr[100], n, item;
 
 public:
 	linear(int n)
 	{
-		cout << "Enter the Array Elements : ";
+		item = n = 0;
+	}
+	int search(int data)
+	{
+		for (int i = 0; i < n; i++)
+			if (arr[i] == item)
+				return i + 1;
+		return n + 1;
+	}
+	void read_array()
+	{
+		cout << "Enter the values of array";
 		for (int i = 0; i < n; i++)
 		{
 			cin >> arr[i];
 		}
-		cout << "Enter the Item : ";
-		cin >> item;
-		int pos = search(arr, n, item);
-		if (pos > n)
-		{
-			cout << "Invalid Search";
-		}
-		else
-		{
-			cout << pos;
-		}
-	}
-	int search(int a[], int n, int data)
-	{
-		for (int i = 0; i < n; i++)
-			if (a[i] == item)
-				return i + 1;
-		return n + 1;
 	}
 };
 int main()
@@ -38,5 +31,12 @@ int main()
 	cout << "Enter the Size of the Array : ";
 	cin >> n;
 	linear l(n);
+	l.read_array();
+
+	int item;
+	cout << "Enter the value to search";
+	cin >> item;
+	int ret = l.search(item);
+	cout << "The item is found at " << ret << " position";
 	return 0;
 }
