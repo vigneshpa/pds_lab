6278a3c4ef27a585ba55bd1eef46d2023695f46e
vignesh <vignesh-p@outlook.com>
2021-10-29 10:05:07 +0530

    friend without dynamic memory allocation

---
+++ ex2.3.simple.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+class Number {
+  int ar[50], n;
+public:
+  friend float find_mean(Number &a);
+  Number(){
+    cout << "N: ";
+    cin >> n;
+    cout << "Values: ";
+    for(int i=0;i<n;i++)
+      cin >> ar[i];
+  };
+};
+float find_mean(Number &a){
+  float sum = 0;
+  for(int i=0;i<a.n;i++)
+    sum += a.ar[i];
+  float mean = (float)sum / a.n;
+}
+int main(){
+  Number a;
+  cout << "Mean: " << find_mean(a) << endl;
+}
