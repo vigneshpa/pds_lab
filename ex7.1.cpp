c119b024931280da2659fbc2af0926ecefebfb4d
vignesh <52572934+vigneshpa@users.noreply.github.com>
2021-12-22 20:17:55 +0530

    Update ex7.1.cpp

--- ex7.1.cpp
+++ ex7.1.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 using namespace std;
-#define MAX 1000
 class Stack {
 public:
-  int arr[MAX], top;
+  int arr[100], top;
 
   Stack() { top = -1; }
   void push(int data) { arr[++top] = data; }
